3ff0255e61a4ff24e277231b60e7bb1f538acc02
joseaznar <jose.aznar94@gmail.com>
2017-07-08 18:49:22 -0500

    Consume from the NYT API

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Noticia(models.Model):
+	id = models.AutoField(primary_key=True)
+	title = models.CharField(max_length = 100)
+	url = models.CharField(max_length = 200)
+	published_date = models.DateTimeField(auto_now = True)
+
+	def __str__(self):
+		return self.title
--- home/views.py
+++ home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 def index(request):
 	noticias = [
-		{'title':"Buscar x en google", 'link':"https://www.google.com/search?q=x&oq=x&aqs=chrome.0.69i59l2j69i60j35i39j0l2.103j0j7&sourceid=chrome&ie=UTF-8"},
-		{'title':"noticia NYT", 'link':"https://www.nytimes.com/2017/07/07/world/europe/trump-putin-g20.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=a-lede-package-region&region=top-news&WT.nav=top-news"}
+		{'title':"Buscar x en google", 'link':"https://www.google.com/search?q=x&oq=x&aqs=chrome.0.69i59l2j69i60j35i39j0l2.103j0j7&sourceid=chrome&ie=UTF-8", 'fecha_fetch':"07/07/17"},
+		{'title':"noticia NYT", 'link':"https://www.nytimes.com/2017/07/07/world/europe/trump-putin-g20.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=a-lede-package-region&region=top-news&WT.nav=top-news", 'fecha_fetch':"07/07/17"}
 	]
+	url = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=08222fe6cb434ccc84799957b2dfeeae'
+	response = requests.get(url)
+	req = response.json()
+	noticias = req['results']
 	return render(request, 'home/index.html', {'noticias':noticias})
