fcc70b3c2ace7334909e736a5bf0e172d56dcb95
joseaznar <jose.aznar94@gmail.com>
2017-07-08 19:35:25 -0500

    Create the Noticia model, put the news in the DB and show them from it

--- home/models.py
+++ home/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Noticia(models.Model):
-	id = models.AutoField(primary_key=True)
-	title = models.CharField(max_length = 100)
-	url = models.CharField(max_length = 200)
-	published_date = models.DateTimeField(auto_now = True)
 
-	def __str__(self):
-		return self.title
+
+class Noticia(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=200, primary_key=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
--- home/views.py
+++ home/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 import requests
+from .models import *
+from django.db import DatabaseError
 # Create your views here.
+
+
 def index(request):
-	noticias = [
-		{'title':"Buscar x en google", 'link':"https://www.google.com/search?q=x&oq=x&aqs=chrome.0.69i59l2j69i60j35i39j0l2.103j0j7&sourceid=chrome&ie=UTF-8", 'fecha_fetch':"07/07/17"},
-		{'title':"noticia NYT", 'link':"https://www.nytimes.com/2017/07/07/world/europe/trump-putin-g20.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=a-lede-package-region&region=top-news&WT.nav=top-news", 'fecha_fetch':"07/07/17"}
-	]
-	url = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=08222fe6cb434ccc84799957b2dfeeae'
-	response = requests.get(url)
-	req = response.json()
-	noticias = req['results']
-	return render(request, 'home/index.html', {'noticias':noticias})
+    noticias = [
+        {'title': "Buscar x en google", 'link': "https://www.google.com/search?q=x&oq=x&aqs=chrome.0.69i59l2j69i60j35i39j0l2.103j0j7&sourceid=chrome&ie=UTF-8", 'fecha_fetch': "07/07/17"},
+        {'title': "noticia NYT", 'link': "https://www.nytimes.com/2017/07/07/world/europe/trump-putin-g20.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=a-lede-package-region&region=top-news&WT.nav=top-news", 'fecha_fetch': "07/07/17"}
+    ]
+    url = 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=08222fe6cb434ccc84799957b2dfeeae'
+    response = requests.get(url)
+    req = response.json()
+    noticias = req['results']
+    count = 0
+    for noticia in noticias:
+        title = noticia['title']
+        url = noticia['url']
+        date = noticia['published_date']
+        try:
+            query = Noticia(title=title, url=url, published_date=date)
+            query.save()
+        except DatabaseError:
+            count = count + 1
+    noticias = Noticia.objects.all()
+    return render(request, 'home/index.html', {'noticias': noticias})
